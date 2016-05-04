7f64816ac571d99372dbf10571c67df9c8278bdd
Maruyama Takayuki <bis5.wsys@gmail.com>
2016-05-04 17:23:17 +0900

    update AssemblyInfo.cs

--- TimeLogger/Properties/AssemblyInfo.cs
+++ TimeLogger/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TimeLogger")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016 Maruyama Takayuki")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
